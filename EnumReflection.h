1f8f074bcdc927b1fcaaeac7123653b34f37489c
Сергей Трубников <bugrazoid@yandex.ru>
2020-07-24 19:29:22 +0300

    Add custom enum underlying type support

--- EnumReflection.h
+++ EnumReflection.h
@@ -5,16 +5,18 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <cassert>
 
 //-------------------------------- Public Interface --------------------------------
 
 // Declare an enumeration inside a class
-#define Z_ENUM( enumName, ... )\
-                              Z_ENUM_DETAIL_MAKE( class, enumName, __VA_ARGS__ )
+#define Z_ENUM( enumName, enumType, ... )\
+                              Z_ENUM_DETAIL_MAKE( class, enumName, enumType, __VA_ARGS__ )
 
 // Declare an enumeration inside a namespace
-#define Z_ENUM_NS( enumName, ... )\
-                              Z_ENUM_DETAIL_MAKE( namespace, enumName, __VA_ARGS__ )
+#define Z_ENUM_NS( enumName, enumType, ... )\
+                              Z_ENUM_DETAIL_MAKE( namespace, enumName, enumType, __VA_ARGS__ )
 
 // Provides access to information about an enum declared with Z_ENUM or Z_ENUM_NS
 class EnumReflector
@@ -88,11 +90,21 @@ public:
 
 public:
     // Constructor. Used internally by Z_ENUM and Z_ENUM_NS
-    EnumReflector(const int*, int, const char*, const char*);
+    template<typename EnumType>
+    EnumReflector(const EnumType*, size_t, const char*, const char*);
     EnumReflector(EnumReflector&&);
     ~EnumReflector();
 private:
-    struct Private;
+    struct Private
+    {
+        struct Enumerator
+        {
+            std::string name;
+            int value;
+        };
+        std::vector<Enumerator> values;
+        std::string enumName;
+    };
     Private* _data;
 };
 
@@ -103,29 +115,29 @@ private:
     inline
 #define Z_ENUM_DETAIL_SPEC_class friend
 #define Z_ENUM_DETAIL_STR(x) #x
-#define Z_ENUM_DETAIL_MAKE(spec, enumName, ...) \
-    enum enumName:int                                                                           \
+#define Z_ENUM_DETAIL_MAKE(spec, enumName, enumType, ...) \
+    enum enumName:enumType                                                                           \
     {                                                                                           \
         __VA_ARGS__                                                                             \
     };                                                                                          \
     Z_ENUM_DETAIL_SPEC_##spec const ::EnumReflector& _detail_reflector_(enumName)               \
     {                                                                                           \
         static const ::EnumReflector _reflector( []{                                            \
-            static int _detail_sval;                                                            \
+            static enumType _detail_sval;                                                            \
             _detail_sval = 0;                                                                   \
             struct _detail_val_t                                                                \
             {                                                                                   \
                 _detail_val_t(const _detail_val_t& rhs) : _val(rhs) { _detail_sval = _val + 1; }\
-                _detail_val_t(int val)                  : _val(val) { _detail_sval = _val + 1; }\
+                _detail_val_t(enumType val)                  : _val(val) { _detail_sval = _val + 1; }\
                 _detail_val_t()                 : _val(_detail_sval){ _detail_sval = _val + 1; }\
                                                                                                 \
                 _detail_val_t& operator=(const _detail_val_t&) { return *this; }                \
-                _detail_val_t& operator=(int) { return *this; }                                 \
-                operator int() const { return _val; }                                           \
-                int _val;                                                                       \
+                _detail_val_t& operator=(enumType) { return *this; }                                 \
+                operator enumType() const { return _val; }                                           \
+                enumType _val;                                                                       \
             } __VA_ARGS__;                                                                      \
-            const int _detail_vals[] = { __VA_ARGS__ };                                         \
-            return ::EnumReflector( _detail_vals, sizeof(_detail_vals)/sizeof(int),             \
+            const enumType _detail_vals[] = { __VA_ARGS__ };                                         \
+            return ::EnumReflector( _detail_vals, sizeof(_detail_vals)/sizeof(enumType),             \
                     #enumName, Z_ENUM_DETAIL_STR((__VA_ARGS__))  );                             \
         }() );                                                                                  \
         return _reflector;                                                                      \
@@ -143,6 +155,81 @@ template<typename EnumType>
 inline const EnumReflector & EnumReflector::For(EnumType val)
 {
     return _detail_reflector_(val);
+}
+
+static bool IsIdentChar(char c)
+{
+    return (c >= 'A' && c <= 'Z') ||
+           (c >= 'a' && c <= 'z') ||
+           (c >= '0' && c <= '9') ||
+           (c == '_');
+}
+
+template<typename EnumType>
+EnumReflector::EnumReflector(const EnumType* vals, size_t count, const char* name, const char* body)
+    : _data(new Private)
+{
+    _data->enumName = name;
+    _data->values.resize(count);
+    enum states
+    {
+        state_start, // Before identifier
+        state_ident, // In identifier
+        state_skip, // Looking for separator comma
+    } state = state_start;
+    assert(*body == '(');
+    ++body;
+    const char* ident_start = nullptr;
+    size_t value_index = 0;
+    int level = 0;
+    for (;;)
+    {
+        assert(*body);
+        switch (state)
+        {
+        case state_start:
+            if (IsIdentChar(*body))
+            {
+                state = state_ident;
+                ident_start = body;
+            }
+            ++body;
+            break;
+        case state_ident:
+            if (!IsIdentChar(*body))
+            {
+                state = state_skip;
+                assert(value_index < count);
+                _data->values[value_index].name = std::string(ident_start, body - ident_start);
+                _data->values[value_index].value = vals[value_index];
+                ++value_index;
+            }
+            else
+            {
+                ++body;
+            }
+            break;
+        case state_skip:
+            if (*body == '(')
+            {
+                ++level;
+            }
+            else if (*body == ')')
+            {
+                if (level == 0)
+                {
+                    assert(value_index == count);
+                    return;
+                }
+                --level;
+            }
+            else if (level == 0 && *body == ',')
+            {
+                state = state_start;
+            }
+            ++body;
+        }
+    }
 }
 
 inline EnumReflector::Enumerator EnumReflector::At(int index) const
@@ -167,7 +254,7 @@ inline EnumReflector::Enumerator EnumReflector::end() const
 
 // EnumReflector::Enumerator
 
-inline EnumReflector::Enumerator::Enumerator(const EnumReflector& er, int index) 
+inline EnumReflector::Enumerator::Enumerator(const EnumReflector& er, int index)
     : _er(er)
     , _index(index) 
 {
--- Test.cpp
+++ Test.cpp
@@ -6,7 +6,7 @@ using std::endl;
 
 
 
-Z_ENUM_NS( CardSuit,
+Z_ENUM_NS( CardSuit, int,
     Spades,
     Hearts,
     Diamonds,
@@ -17,7 +17,7 @@ class SomeClass
 {
 public:
     static const int Constant = 100;
-    Z_ENUM( TasteFlags,
+    Z_ENUM( TasteFlags, int,
         None      = 0,
         Salted    = 1 << 0,
         Sour      = 1 << 1,
@@ -30,7 +30,7 @@ public:
 
 namespace SomeNamespace
 {
-    Z_ENUM_NS( Ports,
+    Z_ENUM_NS( Ports, int,
         HTTP  = 80,
         HTTPS = 443,
         SSH   = 22
@@ -47,21 +47,47 @@ namespace AnotherNamespace
     }
 }
 
+namespace ClassEnum
+{
+
+Z_ENUM_NS(Fruit, int8_t,
+          Apple = 0,
+          Orange = 100
+)
+
+}
+
 int main()
 {
-    auto& reflector = EnumReflector::For<SomeClass::TasteFlags>();
-
-    cout << "Enum " << reflector.EnumName() << endl;
-    for (auto& val : reflector)
     {
-        cout << "Value " << val.Name() << " = " << val.Value() << endl;
+        auto& reflector = EnumReflector::For<SomeClass::TasteFlags>();
+
+        cout << "Enum " << reflector.EnumName() << endl;
+        for (auto& val : reflector)
+        {
+            cout << "Value " << val.Name() << " = " << val.Value() << endl;
+        }
+        cout << endl;
     }
-    cout << endl;
-    ///////////////////////
 
     using AnotherNamespace::GetPort;
 
     cout << "HTTPS port: " << GetPort("HTTPS") << endl;
+    cout << endl;
 
     constexpr bool b = std::is_enum<SomeNamespace::Ports>::value;
+
+    {
+        auto& reflector = EnumReflector::For<ClassEnum::Fruit>();
+        std::cout << std::boolalpha;
+        cout << "Enum " << reflector.EnumName()
+             << " with underlying type is int8_t: "
+             << std::is_same_v<int8_t, std::underlying_type_t<ClassEnum::Fruit>>
+             << endl;
+        for (auto& val : reflector)
+        {
+            cout << "Value " << val.Name() << " = " << val.Value() << endl;
+        }
+        cout << endl;
+    }
 }
