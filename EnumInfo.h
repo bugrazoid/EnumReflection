369ccb7fe2a03008ce6d0b60fce0a5a3022155a8
Сергей Трубников <bugrazoid@yandex.ru>
2020-08-28 17:43:13 +0300

    Add all main methods

--- EnumInfo.h
+++ EnumInfo.h
@@ -6,6 +6,8 @@
 #include <map>
 #include <unordered_map>
 #include <type_traits>
+#include <optional>
+#include <stdexcept>
 
 template<typename Enum>
 struct Adaptor
@@ -36,17 +38,28 @@ constexpr bool isIdentChar(char c)
 }
 
 template<typename Enum>
+class EnumInfo;
+
+template<typename Enum>
 struct ParsedData
 {
     constexpr ParsedData(const char* enumName, const Enum* vals, size_t valsCount, const char* rawNames, size_t rawNamesSize)
         : enumName(enumName)
+        , values(vals)
+        , size(valsCount)
     {
         parseNames(vals, valsCount, rawNames, rawNamesSize);
     }
 
+    using iterator = typename std::map<Enum, std::string_view>::const_iterator;
+    friend EnumInfo<Enum>;
+
+private:
     std::string_view enumName;
-    std::map<Enum, std::string_view> nameByVal;
+    std::multimap<Enum, std::string_view> nameByVal;
     std::unordered_map<std::string_view, Enum> valByName;
+    const Enum* values;
+    const size_t size;
 
     constexpr void parseNames(const Enum* vals, size_t valsCount, const char* rawNames, size_t rawNamesSize)
     {
@@ -114,32 +127,6 @@ struct ParsedData
     }
 };
 
-//template<typename Enum>
-//static auto& getParsedData()
-//{
-//    constexpr const char* const enumName = "EEE";
-//    constexpr const Adaptor<Enum> Value = 0, Other = 10;
-//    constexpr const Enum vals[] = {Value = 0, Other = 10};
-//    constexpr const size_t valsCount = sizeof(vals)/sizeof(Enum);
-//    constexpr const char* const rawNames = "Value = 0, Other = 10";
-//    constexpr const size_t rawNamesSize = sizeof ("Value = 0, Other = 10") - 1;
-//    static const ParsedData<Enum, valsCount> parsedData(enumName, vals, rawNames, rawNamesSize);
-//    return parsedData;
-//}
-
-template<typename Enum>
-class EnumInfo
-{
-public:
-
-private:
-    const static ParsedData<Enum> _parsedData;
-};
-
-template<typename Enum>
-const ParsedData<Enum> EnumInfo<Enum>::_parsedData = getParsedData(Enum());
-
-
 // Declare an enumeration inside a class
 #define ENUM_DECLARE( enumName, enumType, ... )\
                               ENUM_INFO_DETAIL_MAKE( class, enumName, enumType, __VA_ARGS__ )
@@ -153,22 +140,294 @@ const ParsedData<Enum> EnumInfo<Enum>::_parsedData = getParsedData(Enum());
     inline
 #define ENUM_INFO_DETAIL_SPEC_class friend
 #define ENUM_INFO_DETAIL_STR(x) #x
-#define ENUM_INFO_DETAIL_MAKE(spec, enumName, enumType, ...) \
-    enum enumName:enumType                                                                           \
-    {                                                                                           \
-        __VA_ARGS__                                                                             \
-    };                                                                                          \
-    ENUM_INFO_DETAIL_SPEC_##spec \
-    const ParsedData<enumName>& getParsedData(enumName = enumName())    \
-    {   \
+#define ENUM_INFO_DETAIL_MAKE(spec, enumName, enumType, ...)\
+    enum enumName:enumType                              \
+    {                                                       \
+        __VA_ARGS__                                             \
+    };                                                              \
+    ENUM_INFO_DETAIL_SPEC_##spec                                        \
+    const ParsedData<enumName>& getParsedData(enumName = enumName())        \
+    {                                                                           \
         constexpr const char* const enumNameStr = ENUM_INFO_DETAIL_STR(enumName);   \
-        const Adaptor<enumName> __VA_ARGS__;    \
-        const enumName vals[] = { __VA_ARGS__ };  \
-        constexpr size_t valsCount = sizeof(vals)/sizeof(enumName);   \
-        constexpr const char* const rawNames = ENUM_INFO_DETAIL_STR((__VA_ARGS__)); \
-        constexpr const size_t rawNamesSize = sizeof (ENUM_INFO_DETAIL_STR((__VA_ARGS__))) - 1; \
-        static const ParsedData<enumName> parsedData(enumNameStr, vals, valsCount, rawNames, rawNamesSize);    \
-        return parsedData;  \
+        const Adaptor<enumName> __VA_ARGS__;                                            \
+        const enumName vals[] = { __VA_ARGS__ };                                            \
+        constexpr size_t valsCount = sizeof(vals)/sizeof(enumName);                             \
+        constexpr const char* const rawNames = ENUM_INFO_DETAIL_STR((__VA_ARGS__));                 \
+        constexpr const size_t rawNamesSize = sizeof (ENUM_INFO_DETAIL_STR((__VA_ARGS__))) - 1;         \
+        static const ParsedData<enumName> parsedData(enumNameStr, vals, valsCount, rawNames, rawNamesSize); \
+        return parsedData;                                                                              \
     }
 
+template<typename Enum>
+class EnumInfo
+{
+public:
+    using EnumType = std::underlying_type_t<Enum>;
+    // TODO: сделать возможность через параметры шаблона задавать используемую строку.
+    static std::string_view      name();
+    static size_t                size();
+    static std::optional<std::string_view> valueName(Enum value);
+    static std::optional<std::string_view> valueName(size_t index);
+    static std::optional<Enum>             value(std::string_view name);
+    static std::optional<Enum>             value(size_t index);
+    static std::optional<size_t>           index(Enum value);
+    static std::optional<size_t>           index(std::string_view name);
+
+    struct iterator
+    {
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type  = iterator;
+        using difference_type = std::ptrdiff_t;
+
+        iterator() = delete;
+        explicit iterator(typename ParsedData<Enum>::iterator it);
+
+        size_t index();
+        Enum value();
+        std::string_view name();
+
+        value_type      operator*();
+        iterator&       operator++();
+        iterator        operator++(difference_type);
+        iterator&       operator--();
+        iterator        operator--(difference_type);
+        bool            operator==(iterator);
+        bool            operator!=(iterator);
+
+    private:
+        typename ParsedData<Enum>::iterator _it;
+        size_t _index;
+    };
+
+    using reverse_iterator = std::reverse_iterator<iterator>;
+
+    static iterator begin();
+    static iterator end();
+
+    static reverse_iterator rbegin();
+    static reverse_iterator rend();
+
+private:
+    const static ParsedData<Enum> _parsedData;
+
+    template<typename Ret, typename Key, typename Cont>
+    static std::optional<Ret> find(Key key, Cont cont);
+    static iterator fromIndex(size_t index);
+    template<typename Key, typename Cont>
+    static std::optional<EnumType> findIndex(Key key, Cont cont);
+};
+
+template<typename Enum>
+const ParsedData<Enum> EnumInfo<Enum>::_parsedData = getParsedData(Enum());
+
 #endif // ENUMINFO_H
+
+// ---- EnumInfo implementation ----
+
+template<typename Enum>
+std::string_view EnumInfo<Enum>::name()
+{
+    return _parsedData.enumName;
+}
+
+template<typename Enum>
+std::optional<std::string_view> EnumInfo<Enum>::valueName(Enum value)
+{
+    return EnumInfo::find<std::string_view>(value, _parsedData.nameByVal);
+}
+
+template<typename Enum>
+std::optional<std::string_view> EnumInfo<Enum>::valueName(size_t index)
+{
+    const iterator it = fromIndex(index);
+    return it.name();
+}
+
+template<typename Enum>
+std::optional<Enum> EnumInfo<Enum>::value(std::string_view name)
+{
+    return EnumInfo::find<Enum>(name, _parsedData.valByName);
+}
+
+template<typename Enum>
+std::optional<Enum> EnumInfo<Enum>::value(size_t index)
+{
+    const iterator it = fromIndex(index);
+    return it.value();
+}
+
+template<typename Enum>
+std::optional<size_t> EnumInfo<Enum>::index(Enum value)
+{
+    return findIndex(value, _parsedData.nameByVal);
+}
+
+template<typename Enum>
+std::optional<size_t> EnumInfo<Enum>::index(std::string_view name)
+{
+    return findIndex(name, _parsedData.valByName);
+}
+
+template<typename Enum>
+size_t EnumInfo<Enum>::size()
+{
+    return _parsedData.size;
+}
+
+template<typename Enum>
+typename EnumInfo<Enum>::iterator EnumInfo<Enum>::begin()
+{
+    return iterator(_parsedData.nameByVal.begin());
+}
+
+template<typename Enum>
+typename EnumInfo<Enum>::iterator EnumInfo<Enum>::end()
+{
+    return iterator(_parsedData.nameByVal.end());
+}
+
+template<typename Enum>
+typename EnumInfo<Enum>::reverse_iterator EnumInfo<Enum>::rbegin()
+{
+    return iterator(_parsedData.nameByVal.begin());
+}
+
+template<typename Enum>
+typename EnumInfo<Enum>::reverse_iterator EnumInfo<Enum>::rend()
+{
+    return iterator(_parsedData.nameByVal.end());
+}
+
+template<typename Enum>
+typename EnumInfo<Enum>::iterator EnumInfo<Enum>::fromIndex(size_t index)
+{
+    iterator it(_parsedData.nameByVal.begin());
+    for (size_t i = 0; i != index; ++i)
+    {
+        ++it;
+    }
+    return it;
+}
+
+template<typename Enum>
+template<typename Ret, typename Key, typename Cont>
+std::optional<Ret> EnumInfo<Enum>::find(Key key, Cont cont)
+{
+    const auto it = cont.find(key);
+    if (it != cont.end())
+    {
+        return it->second;
+    }
+
+    return std::nullopt;
+}
+
+template<typename Enum>
+template<typename Key, typename Cont>
+std::optional<typename EnumInfo<Enum>::EnumType> EnumInfo<Enum>::findIndex(Key key, Cont cont)
+{
+    const auto it = cont.find(key);
+        if (it != cont.end())
+        {
+            size_t i = 0;
+            auto in = cont.begin();
+            while (in != cont.end())
+            {
+                if (in == it)
+                    return i;
+                ++in;
+                ++i;
+            }
+        }
+
+        return std::nullopt;
+}
+
+// ---- EnumInfo::iterator implementation ----
+
+template<typename Enum>
+EnumInfo<Enum>::iterator::iterator(typename ParsedData<Enum>::iterator it)
+    : _it(it)
+    , _index(0)
+{
+    auto current = EnumInfo<Enum>::_parsedData.nameByVal.begin();
+    while (current != _it)
+    {
+        if (_index == EnumInfo<Enum>::size())
+            std::out_of_range("Something gonna wrong!");
+        ++current;
+        ++_index;
+    }
+}
+
+template<typename Enum>
+size_t EnumInfo<Enum>::iterator::index()
+{
+    return _index;
+}
+
+template<typename Enum>
+Enum EnumInfo<Enum>::iterator::value()
+{
+    return _it->first;
+}
+
+template<typename Enum>
+std::string_view EnumInfo<Enum>::iterator::name()
+{
+    return  _it->second;
+}
+
+template<typename Enum>
+typename EnumInfo<Enum>::iterator::value_type EnumInfo<Enum>::iterator::operator*()
+{
+    return *this;
+}
+
+/// Prefix operator++
+template<typename Enum>
+typename EnumInfo<Enum>::iterator& EnumInfo<Enum>::iterator::operator++()
+{
+    ++_it;
+    ++_index;
+    return *this;
+}
+
+/// Postfix operator++
+template<typename Enum>
+typename EnumInfo<Enum>::iterator EnumInfo<Enum>::iterator::operator++(EnumInfo::iterator::difference_type)
+{
+    EnumInfo<Enum>::iterator tmp(*this);
+    operator++();
+    return tmp;
+}
+
+/// Prefix operator--
+template<typename Enum>
+typename EnumInfo<Enum>::iterator& EnumInfo<Enum>::iterator::operator--()
+{
+    --_it;
+    --_index;
+    return *this;
+}
+
+/// Postfix operator--
+template<typename Enum>
+typename EnumInfo<Enum>::iterator EnumInfo<Enum>::iterator::operator--(EnumInfo::iterator::difference_type)
+{
+    EnumInfo<Enum>::iterator tmp(*this);
+    operator--();
+    return tmp;
+}
+
+template<typename Enum>
+bool EnumInfo<Enum>::iterator::operator==(EnumInfo::iterator other)
+{
+    return _it == other._it;
+}
+
+template<typename Enum>
+bool EnumInfo<Enum>::iterator::operator!=(EnumInfo::iterator other)
+{
+    return _it != other._it;
+}
--- Test.cpp
+++ Test.cpp
@@ -2,128 +2,83 @@
 #include "EnumInfo.h"
 
 #include <iostream>
-using std::cout;
-using std::endl;
+#include <limits>
 
 
-
-//Z_ENUM_NS( CardSuit, int,
-//    Spades,
-//    Hearts,
-//    Diamonds,
-//    Clubs
-//)
+ENUM_DECLARE_NS( CardSuit, int8_t,
+    Spades,
+    Hearts,
+    Diamonds,
+    Clubs
+)
 
 class SomeClass
 {
 public:
     static const int Constant = 100;
-//    Z_ENUM( TasteFlags, int,
-//        None      = 0,
-//        Salted    = 1 << 0,
-//        Sour      = 1 << 1,
-//        Sweet     = 1 << 2,
-//        SourSweet = (Sour | Sweet),
-//        Other     = Constant,
-//        Last
-//    )
+    ENUM_DECLARE( TasteFlags, uint64_t,
+        None      = 0,
+        Salted    = 1 << 0,
+        Sour      = 1 << 1,
+        Sweet     = 1 << 2,
+        SourSweet = (Sour | Sweet),
+        Other     = Constant,
+        Last      = std::numeric_limits<uint64_t>::max()
+    )
 };
 
 namespace SomeNamespace
 {
-//    Z_ENUM_NS( Ports, int,
-//        HTTP  = 80,
-//        HTTPS = 443,
-//        SSH   = 22
-//    )
+    ENUM_DECLARE_NS( Ports, int,
+        HTTP  = 80,
+        HTTPS = 443,
+        SecureShell = 22,
+        SSH   = 22
+    )
 
 }
 
 namespace AnotherNamespace
 {
-//    static int GetPort(const std::string& name)
-//    {
-//        const auto& port = EnumReflector::For<::SomeNamespace::Ports>().Find(name);
-//        return port.IsValid() ? port.Value() : 0;
-//    }
+    static int GetPort(const std::string& name)
+    {
+        const auto& port = EnumInfo<::SomeNamespace::Ports>::value(name);
+        return port.has_value() ? port.value() : 0;
+    }
 }
 
-namespace ClassEnum
+template<typename Enum>
+void printEnum()
 {
-
-//Z_ENUM_NS(Fruit, int8_t,
-//          Apple = 0,
-//          Orange = 100
-//)
-
-}
-
-
-class CL1
-{
-public:
-    ENUM_DECLARE(ABC, int,
-                 A = 10, B, C)
-};
-
-ENUM_DECLARE_NS(EEE, int,
-    Value = 0,
-    Other = 10
-);
-
-namespace NS1
-{
-    ENUM_DECLARE_NS(NNN, int,
-                    A = 0,
-                    B,
-                    C)
+    std::cout << "Enum " << EnumInfo<Enum>::name()
+              << " with underlying type " << typeid (typename EnumInfo<Enum>::EnumType).name() << std::endl;
+    for (auto it: EnumInfo<Enum>())
+    {
+        std::cout
+                << it.index() << ") "
+                << std::to_string(it.value()) << " - "
+                << it.name() << std::endl;
+    }
+    std::cout << "Enum size is " << EnumInfo<Enum>::size() << std::endl << std::endl;
 }
 
 
 int main()
 {
-//    {
-//        auto& reflector = EnumReflector::For<SomeClass::TasteFlags>();
+    printEnum<SomeClass::TasteFlags>();
 
-//        cout << "Enum " << reflector.EnumName() << endl;
-//        for (auto& val : reflector)
-//        {
-//            cout << "Value " << val.Name() << " = " << val.Value() << endl;
-//        }
-//        cout << endl;
-//    }
-
-//    using AnotherNamespace::GetPort;
-
-//    cout << "HTTPS port: " << GetPort("HTTPS") << endl;
-//    cout << endl;
-
-//    constexpr bool b = std::is_enum<SomeNamespace::Ports>::value;
-
-//    {
-//        auto& reflector = EnumReflector::For<ClassEnum::Fruit>();
-//        std::cout << std::boolalpha;
-//        cout << "Enum " << reflector.EnumName()
-//             << " with underlying type is int8_t: "
-//             << std::is_same_v<int8_t, std::underlying_type_t<ClassEnum::Fruit>>
-//             << endl;
-//        for (auto& val : reflector)
-//        {
-//            cout << "Value " << val.Name() << " = " << val.Value() << endl;
-//        }
-//        cout << endl;
-//    }
-
-    EnumInfo<CL1::ABC> abc;
-    EnumInfo<CL1::ABC> abc1;
-    EnumInfo<CL1::ABC> abc2;
-
-    const EEE aa[] = {Value, Other};
-    const auto ba = getParsedData(CL1::ABC());
-    std::cout << ba.enumName << std::endl;
-    for (const auto& b: ba.nameByVal)
-    {
-        std::cout << b.first << " " << b.second << std::endl;
-    }
+    std::cout << "Index for " << EnumInfo<SomeClass::TasteFlags>::valueName(SomeClass::TasteFlags::Sour).value_or("ERROR")
+              << " is " << EnumInfo<SomeClass::TasteFlags>::index(SomeClass::TasteFlags::Sour).value_or(-1) << std::endl;
     std::cout << std::endl;
+
+    printEnum<SomeNamespace::Ports>();
+
+    using AnotherNamespace::GetPort;
+
+    std::cout << "HTTPS port: " << GetPort("HTTPS") << std::endl;
+    std::cout << std::to_string(SomeNamespace::Ports::SSH) << " is port for "
+              << EnumInfo<SomeNamespace::Ports>::valueName(SomeNamespace::Ports::SSH).value_or("ERROR") << std::endl;
+    std::cout << std::endl;
+
+    printEnum<CardSuit>();
 }
